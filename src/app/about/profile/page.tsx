d2c5beb3cee8ef08a11e2468fbf6ade5b94033a2
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-23 17:54:07 +0700

    try to create nested route

---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./navbar";
+
+export default function ProfilePage() {
+  return (
+    <>
+    <Navbar/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Profile Page
+    </main>
+    </>
+  );
+}
