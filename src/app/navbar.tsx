700fbbb4f5ba984c1037d1955c8c94a50b19a158
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-24 09:49:17 +0700

    add(navbar): event tab active with link element

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return(
     <nav className="flex items-center justify-between px-6 py-4">
       <h1>Navbar</h1>
       <ul className="flex gap-5 items-center text-sm cursor-pointer">
         <Link href="/">
-        <li>Home</li>
+        <li className={`${pathname === '/' ? "text-blue-600" : "text-white"}`}>Home</li>
         </Link>
         <Link href="/about">
-        <li>About</li>
+        <li className={`${pathname === '/about' ? "text-blue-600" : "text-white"}`}>About</li>
         </Link>
         <Link href="/about/profile">
-        <li>Profile</li>
+        <li className={`${pathname === '/about/profile' ? "text-blue-600" : "text-white"}`}>Profile</li>
         </Link>
       </ul>
     </nav>
