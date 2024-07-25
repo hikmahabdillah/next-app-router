1dd6d36036b77ebea1a8895cf83e400c5b9c4e59
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-25 18:29:24 +0700

    feat(middleware): add middleware if not logged in

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -4,7 +4,7 @@ type ProductProps = {params: { slug: string[]}}
 
 async function getData(){
   // const res = await fetch('https://fakestoreapi.com/products');
-  const res = await fetch('http://localhost:3000/api/xproduct', {
+  const res = await fetch('http://localhost:3000/api/product', {
     cache: "force-cache",
     next: { 
       // revalidate: 30 
