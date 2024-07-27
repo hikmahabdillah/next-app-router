29467ae99c5da207c46f0d8a4b523b7fc38dfc2d
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-27 15:46:49 +0700

    feat(lib): connect to database

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import {retrieveData, retrieveDataById} from "@/lib/firebase/service"
 interface Product {
-  id: number;
+  id: string;
   title: string;
   category: string;
   price: number;
@@ -11,61 +11,59 @@ interface Product {
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url); //get id from params
   const id = searchParams.get("id");
-  const dataProducts: Product[] = [
-    {
-      id: 1,
-      title: "Ultraboost 5x Shoes",
-      category: "Shoes",
-      price: 200,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/ffbe5d912e2813a20bb27461a01c1e7c/j/i/ji1334_2_footwear_photography_side_lateral_view_grey.jpg",
-    },
-    {
-      id: 2,
-      title: "adidas Sleek Shoes",
-      category: "Shoes",
-      price: 100,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
-    },
-    {
-      id: 3,
-      title: "Response Super Shoes",
-      category: "Shoes",
-      price: 150,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/j/i/ji4317_2_footwear_photography_side20lateral20view_grey.jpg",
-    },
-    {
-      id: 4,
-      title: "Response Super Shoes",
-      category: "Shoes",
-      price: 150,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/j/i/ji4317_2_footwear_photography_side20lateral20view_grey.jpg",
-    },
-    {
-      id: 5,
-      title: "adidas Sleek Shoes",
-      category: "Shoes",
-      price: 100,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
-    },
-    {
-      id: 6,
-      title: "adidas Sleek Shoes",
-      category: "Shoes",
-      price: 100,
-      image:
-        "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
-    },
-  ];
+  // const dataProducts: Product[] = [
+  //   {
+  //     id: 1,
+  //     title: "Ultraboost 5x Shoes",
+  //     category: "Shoes",
+  //     price: 200,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/ffbe5d912e2813a20bb27461a01c1e7c/j/i/ji1334_2_footwear_photography_side_lateral_view_grey.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "adidas Sleek Shoes",
+  //     category: "Shoes",
+  //     price: 100,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Response Super Shoes",
+  //     category: "Shoes",
+  //     price: 150,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/j/i/ji4317_2_footwear_photography_side20lateral20view_grey.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Response Super Shoes",
+  //     category: "Shoes",
+  //     price: 150,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/j/i/ji4317_2_footwear_photography_side20lateral20view_grey.jpg",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "adidas Sleek Shoes",
+  //     category: "Shoes",
+  //     price: 100,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "adidas Sleek Shoes",
+  //     category: "Shoes",
+  //     price: 100,
+  //     image:
+  //       "https://www.adidas.co.id/media/catalog/product/cache/3bec5fdb79d91223b1a151be2b21ce8d/i/h/ih1298_2_footwear_photography_side20lateral20view_grey.jpg",
+  //   },
+  // ];
 
   if (id) {
-    const detailProduct: Product | undefined = dataProducts.find(
-      (item: Product) => item.id === Number(id)
-    );
+    const detailProduct: Product | undefined = await retrieveDataById("products", id);
     if (detailProduct) {
       return NextResponse.json({
         status: 200,
@@ -75,10 +73,13 @@ export async function GET(request: NextRequest) {
     }
     return NextResponse.json({ status: 404, message: "Not Found", data: {} });
   } else {
+
+    const products = await retrieveData("products");
     return NextResponse.json({
       status: 200,
       message: "Success",
-      data: dataProducts,
+      data: products,
     });
   }
+
 }
---
+++ src/lib/firebase/init.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAp6VCsMSJEomr3R183EWfSALMmKjqNW1E",
+  authDomain: "next-app-router-4defd.firebaseapp.com",
+  projectId: "next-app-router-4defd",
+  storageBucket: "next-app-router-4defd.appspot.com",
+  messagingSenderId: "375046223413",
+  appId: "1:375046223413:web:248e5e6fa35c9fdb76a37c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
