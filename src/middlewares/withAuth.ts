6dac1a0b9100573d9393793cac55c946ebcd9a64
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-28 11:40:15 +0700

    feat(auth): login multi role and add access rights

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,6 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
 
+const onlyAdmin = ['/dashboard'];
+
 export default function withAuth(middleware: NextMiddleware, requireAuth: string[] = []){
   return async (req: NextRequest, next: NextFetchEvent) => {
     const pathname = req.nextUrl.pathname;
@@ -15,6 +17,11 @@ export default function withAuth(middleware: NextMiddleware, requireAuth: string
         const url = new URL('/login', req.url);
         url.searchParams.set('callbackUrl', encodeURI(req.url)); // returns to the last opened url
         return NextResponse.redirect(url);
+      }
+      
+      if(token.role !== 'admin' && onlyAdmin.includes(pathname)){
+        // if user role not admin, redirect to home page
+        return NextResponse.redirect(new URL('/', req.url));
       }
     }
     return middleware(req, next);
